801e96173b178288f414987f03b8c85c7ebb36f9
Mehmet Efe Akça <efeakca@gmail.com>
2021-10-11 23:39:53 +0300

    add device button

--- src/routes/User.tsx
+++ src/routes/User.tsx
@@ -1,26 +1,26 @@
-import { doc } from "@firebase/firestore"
-import { getDoc } from "firebase/firestore"
-import { useCallback, useEffect, useState } from "react"
-import { useFirestore } from "../util/firebase"
-import { getFunctions, httpsCallable } from "firebase/functions"
-import { getAuth } from "firebase/auth"
-import { useAppSelector } from "../store"
+import { doc } from "@firebase/firestore";
+import { getDoc } from "firebase/firestore";
+import { useCallback, useEffect, useState } from "react";
+import { useFirestore } from "../util/firebase";
+import { getFunctions, httpsCallable } from "firebase/functions";
+import { getAuth } from "firebase/auth";
+import { useAppSelector } from "../store";
 
 export const User: React.FC = () => {
-  const fs = useFirestore()
-  const auth = getAuth()
-  const [user, setUser] = useState(null)
+  const fs = useFirestore();
+  const auth = getAuth();
+  const [user, setUser] = useState(null);
   useEffect(() => {
     getDoc(doc(fs, `users/${auth.currentUser.uid}`)).then((s): void =>
       setUser(s.data())
-    )
-  }, [auth.currentUser?.uid, fs])
-  const devices = user?.devices
-  const token = useAppSelector((s) => s.deviceToken)
+    );
+  }, [auth.currentUser?.uid, fs]);
+  const devices = user?.devices;
+  const token = useAppSelector((s) => s.deviceToken);
   const addThisDevice = useCallback(
     () => httpsCallable(getFunctions(), "addDevice")({ token }),
     [token]
-  )
+  );
 
   return (
     <div className="relative flex-row container z-10 bg-white text-black flex font-semibold rounded-lg border shadow-lg px-10 py-8 m-20">
@@ -46,8 +46,22 @@ export const User: React.FC = () => {
             </button>
           </div>
         ) : null}
+        {devices &&
+        devices.findIndex((d: { token: string }) => d.token === token) ===
+          -1 ? (
+          <div>
+            Bu cihaz bulunamadı. Bu cihazı eklemek için{" "}
+            <button
+              className="inline underline font-bold cursor-pointer"
+              onClick={addThisDevice}
+            >
+              {" "}
+              buraya tıkla.
+            </button>
+          </div>
+        ) : null}
         <div className="flex-1" />
       </div>
     </div>
-  )
-}
+  );
+};
